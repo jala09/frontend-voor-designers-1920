8b8135846a833e392d35f0505ed9b33e0914f292
jala09 <38378888+jala09@users.noreply.github.com>
2020-02-27 17:01:25 +0100

    last changes drag

--- opdracht1/js/script.js
+++ opdracht1/js/script.js
@@ -4,3 +4,58 @@
   2. Koppel een evenListener aan het element om een mouse-event te detecteren
   3. Gebruik het Classlist object om een css class aan een element toe te voegen of weg te halen.
 */
+
+function enableDragSort(listClass) {
+    const sortableLists = document.getElementsByClassName(listClass);
+    Array.prototype.map.call(sortableLists, (list) => {
+        enableDragList(list)
+    });
+}
+
+function enableDragList(list) {
+    Array.prototype.map.call(list.children, (item) => {
+        enableDragItem(item)
+    });
+
+}
+
+function enableDragItem(item) {
+    item.setAttribute('draggable', true)
+    item.ondrag = handleDrag;
+    item.ondragend = handleDrop;
+
+
+}
+
+
+
+
+function handleDrag(item) {
+
+    const selectedItem = item.target,
+        list = selectedItem.parentNode,
+        x = event.clientX,
+        y = event.clientY;
+
+    selectedItem.classList.add('drag-sort-active');
+    let swapItem = document.elementFromPoint(x, y) === null ? selectedItem : document.elementFromPoint(x, y);
+
+
+
+    if (list === swapItem.parentNode) {
+        swapItem = swapItem !== selectedItem.nextSibling ? swapItem : swapItem.nextSibling;
+        list.insertBefore(selectedItem, swapItem);
+    }
+
+     document.querySelector('li').classList.add("li");
+}
+
+
+
+function handleDrop(item) {
+    item.target.classList.remove('drag-sort-active');
+}
+
+(() => {
+    enableDragSort('list')
+})();
